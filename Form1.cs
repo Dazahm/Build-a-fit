69e5741c902762bfbce250361a363de05e884bc0
Dazahm <dazahmitchell2@gmail.com>
2025-06-08 12:11:46 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_upload(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_weather(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_calendar(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_settings(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pic_box(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
